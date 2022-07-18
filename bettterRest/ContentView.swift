89a12c0f80dd66c5fa009b8fa57c6654ec7b5212
adarsh <adarsh0ranjan@gmail.com>
2022-07-18 09:42:56 +0530

    Initial Commit

---
+++ bettterRest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bettterRest
+//
+//  Created by Roro Solutions on 18/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ bettterRest/bettterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  bettterRestApp.swift
+//  bettterRest
+//
+//  Created by Roro Solutions on 18/07/22.
+//
+
+import SwiftUI
+
+@main
+struct bettterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
