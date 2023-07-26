2e705129951662e7a8966297358737bf9a9227e3
adarsh <adarsh0ranjan@gmail.com>
2023-07-26 13:24:27 +0530

    Revert "second commit"
    This reverts commit 24404970bcf52c659e898a2f4ae2e75b6fdc09a8.

--- bettterRest/ContentView.swift
+++ bettterRest/ContentView.swift
@@ -14,10 +14,6 @@ struct ContentView: View {
     @State private var alertTitle = ""
     @State private var alertMessage = ""
     @State private var showingAlert = false
-    var correctedCoffeeAmount: Int {
-            coffeeAmount + 1
-        }
-   
     
     static var defaultWakeTime: Date {
         var components = DateComponents()
@@ -29,57 +25,49 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView{
-            List{
-                Section{
-                    DatePicker("Please enteer a time", selection: $wakeUp,displayedComponents: .hourAndMinute)
-                        
-                }header: {
-                    Text("When do you want to wake up?")
-                        .font(.headline)
-                }
-                Section{
-                    Stepper("\(sleepAmount.formatted()) hours",value: $sleepAmount,in: 4...12, step: 0.25)
-                }header: {
-                    Text("Desird amount of sleeep")
-                        .font(.headline)
-                }
-                Section("Daily coffee intake") {
-                    Picker(correctedCoffeeAmount == 1 ? "1 cup" : "\(correctedCoffeeAmount) cups", selection: $coffeeAmount) {
-                        ForEach(1..<21) {
-                            Text("\($0)")
-                        }
-                    }
-                }
-                VStack{
-                    Text("Your ideal bedtime is...")
-                        .font(.subheadline)
-                    Text(calculateBedtime.formatted(date: .omitted, time: .shortened))
-                        .font(.largeTitle)
-                        .bold()
-                }
+            VStack{
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                DatePicker("Please enteer a time", selection: $wakeUp,displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                Text("Desird amount of sleeep")
+                    .font(.headline)
+                Stepper("\(sleepAmount.formatted()) hours",value: $sleepAmount,in: 4...12, step: 0.25)
+                Text("Amounnt of coffee intake?")
+                    .font(.headline)
+                Stepper(coffeeAmount == 1 ? "1 cup" : " \(coffeeAmount) cups",value: $coffeeAmount,in: 1...12, step: 1)
             }
-            
-            
+            .padding()
             .navigationTitle("BtteerRest")
-            
+            .toolbar {
+                Button("Calculate", action: calculateBedtime)
+            }
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("OK") { }
+            } message: {
+                Text(alertMessage)
+            }
                     
         }
     }
-    var  calculateBedtime: Date{
+    func calculateBedtime(){
         do {
             let config = MLModelConfiguration()
             let model = try sleepCalculatttor(configuration: config)
             let components = Calendar.current.dateComponents([.hour,.minute], from: wakeUp)
             let hour = (components.hour) ?? 0 * 60 * 60
             let minute = (components.minute ?? 0) * 60
-            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount + 1))
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
             let sleepTime = wakeUp - prediction.actualSleep
-            return sleepTime
+            alertTitle = "Your ideal bedtime is…"
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
             // more code here
         } catch {
-            return Date.now
+            // something went wrong!
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
         }
-        
+        showingAlert = true
         
     }
 }
